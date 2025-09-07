fa4c8d33474177d4821a8c57558eb0ab7e06e02a
4lfiannur <4lfian.nur@gmail.com>
2025-09-07 17:19:45 +0800

    fix(midtrans): align config keys (server_key/sanitize) & remove unused

--- config/midtrans.php
+++ config/midtrans.php
@@ -1,12 +1,12 @@
 <?php
 
 return [
-    'merchant_id' => env('MIDTRANS_MERCHANT_ID'),
-    'merchant_key' => env('MIDTRANS_MERCHANT_KEY'),
-    'merchant_secret' => env('MIDTRANS_MERCHANT_SECRET'),
-    'is_production' => env('MIDTRANS_IS_PRODUCTION', false),
+    'merchant_id'  => env('MIDTRANS_MERCHANT_ID'),
+    'client_key'   => env('MIDTRANS_CLIENT_KEY'),
+    'server_key'   => env('MIDTRANS_SERVER_KEY'),
+    'is_production'=> env('MIDTRANS_IS_PRODUCTION', false),
     'is_sanitized' => true,
-    'enable_3ds' => true,
+    'enable_3ds'   => true,
 
     'payment_url' => env('MIDTRANS_IS_PRODUCTION', false)
         ? 'https://app.midtrans.com/snap/snap.js'
